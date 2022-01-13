4c21578d466e317f66a8c13d5e3bb93ec0b99fa1
Marco Toniut <marco.toniut@gmail.com>
2022-01-13 02:13:01 -0300

    Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// All available options are documented here:
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  arrowParens: "always",
+  overrides: [
+    // Override settings for specific file types here
+  ],
+};

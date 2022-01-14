b66e22b4befb6a3110f2d784ce32b9818d8fe2af
Marco Toniut <marco.toniut@gmail.com>
2022-01-14 08:05:27 -0300

    @react-hooke/react: Update `useDependent`'s description

--- packages/react/src/useDependent.ts
+++ packages/react/src/useDependent.ts
@@ -3,9 +3,10 @@ import { areHookInputsEqual } from "./__internal__";
 
 /**
  * `useDependent` returns a lazy value that will be re-computed on each change in the dependency array.
+ * @note The api is virtually equivalent to [`useMemo`](https://reactjs.org/docs/hooks-reference.html#usememo)'s,
+ * the difference being that `useDependent` won't forget values, making it a good candidate for **semantic guarantees**
  * @param create The lazy value yielded on each render.
  * @returns A `value` re-computed every time a dependency changes.
- * @notes At one point and, for the love of Zeus, somebody unit test this!
  */
 export function useDependent<S>(
   create: () => S,
